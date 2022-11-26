958c4f0a0e69d7d1dec0cf8992c8e9abf7e29eb5
علی خداوردی <alikhodaveerdi@yahoo.com>
2022-11-26 19:53:30 +0330

    create-controller-superhero

---
+++ SuperHeroAPI-NET-7/Controllers/SuperHeroController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI_NET_7.Models;
+
+namespace SuperHeroAPI_NET_7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllHeroes()
+        {
+            var superHeroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+            return Ok(superHeroes);
+        }
+    }
+}
