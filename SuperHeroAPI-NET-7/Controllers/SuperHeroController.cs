b87574593289fd2119734bdde94f8358e1adc9f7
علی خداوردی <alikhodaveerdi@yahoo.com>
2022-11-26 19:56:44 +0330

    test-get-method-superhero

--- SuperHeroAPI-NET-7/Controllers/SuperHeroController.cs
+++ SuperHeroAPI-NET-7/Controllers/SuperHeroController.cs
@@ -9,7 +9,7 @@ namespace SuperHeroAPI_NET_7.Controllers
     public class SuperHeroController : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> GetAllHeroes()
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
         {
             var superHeroes = new List<SuperHero>
             {
